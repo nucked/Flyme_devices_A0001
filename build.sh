2f5a48d603a455b877a6e0fe8260faf877f5d3b6
nucked <1286312989@qq.com>
2015-09-29 16:13:59 +0800

    添加supersu，基带等

---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+cd ../..
+echo
+echo ">>> 初始化编译环境"
+. build/envsetup.sh
+echo
+echo
+echo ">>> 进入机型目录"
+cd devices/A0001
+pwd
+echo
+echo
+echo ">>> 清除缓存文件"
+flyme clean
+echo ">>> 编译刷机包"
+flyme fullota
+
+if [ -e ./out/flyme*.zip ]
+   then
+mkdir -p out/flyme
+mv ./out/flyme*.zip ./out/flyme
+cd ./out/flyme
+unzip flyme*.zip
+rm -rf flyme*.zip
+cp -rf ../../patch/updater-script.patch ./
+patch -p1 < updater-script.patch
+rm -rf updater-script.patch
+cp -rf ../../patch/SuperSU/* ./
+zip -r flyme.zip *
+java -jar ../../../../build/tools/signapk.jar ../../../../build/security/platform.x509.pem ../../../../build/security/platform.pk8 flyme.zip flyme_A0001_Nucked_$(date +%Y%m%d).zip
+mv flyme*.zip ../
+rm -rf ../flyme
+rm -rf ../flyme.zip
+echo
+echo "刷机包编译完成！刷机包在out目录中"
+cd ..
+pwd
+echo
+fi
