2711f604bc24801c051f766eb7bf53805bb1ac95
Nucked <1286312989@qq.com>
2015-11-29 13:05:05 +0800

    update baseos YOG7D

--- vendor/system/bin/install-recovery.sh
+++ vendor/system/bin/install-recovery.sh
@@ -1,10 +1,10 @@
 #!/system/bin/sh
 if [ -f /system/etc/recovery-transform.sh ]; then
-  exec sh /system/etc/recovery-transform.sh 10723328 f63014ce462aaffde4c58d91c7261f4fa24317bd 7983104 6b66e5430f449f269f2aa15b9c83f9be152d22f0
+  exec sh /system/etc/recovery-transform.sh 10743808 282020861d30985e498802a42277a796e0f11528 7993344 232a6b9e8fe547799078c09dc7ddb44a604c05d3
 fi
 
-if ! applypatch -c EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery:10723328:f63014ce462aaffde4c58d91c7261f4fa24317bd; then
-  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/platform/msm_sdcc.1/by-name/boot:7983104:6b66e5430f449f269f2aa15b9c83f9be152d22f0 EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery f63014ce462aaffde4c58d91c7261f4fa24317bd 10723328 6b66e5430f449f269f2aa15b9c83f9be152d22f0:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery:10743808:282020861d30985e498802a42277a796e0f11528; then
+  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/platform/msm_sdcc.1/by-name/boot:7993344:232a6b9e8fe547799078c09dc7ddb44a604c05d3 EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery 282020861d30985e498802a42277a796e0f11528 10743808 232a6b9e8fe547799078c09dc7ddb44a604c05d3:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
