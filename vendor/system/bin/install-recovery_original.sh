02d86df5ebf10d6715147ca2ce6a9f4e0e28a8ca
nucked <1286312989@qq.com>
2016-01-07 01:18:25 +0800

    Upgrade 12.1-YOG7D-20160105-STABLE-Sultan-bacon

--- vendor/system/bin/install-recovery_original.sh
+++ vendor/system/bin/install-recovery_original.sh
@@ -1,10 +1,10 @@
 #!/system/bin/sh
 if [ -f /system/etc/recovery-transform.sh ]; then
-  exec sh /system/etc/recovery-transform.sh 10901504 2b053ad07eedd2836b497695c49e274bcd28252d 8151040 3c3de4091302f225a51c8e4b5bab6075a1671045
+  exec sh /system/etc/recovery-transform.sh 10901504 09b9ea9ca34c0b086701d15d270453f404b6b1e2 8151040 090d8248b641715f52a908903dc61d890c3bce65
 fi
 
-if ! applypatch -c EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery:10901504:2b053ad07eedd2836b497695c49e274bcd28252d; then
-  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/platform/msm_sdcc.1/by-name/boot:8151040:3c3de4091302f225a51c8e4b5bab6075a1671045 EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery 2b053ad07eedd2836b497695c49e274bcd28252d 10901504 3c3de4091302f225a51c8e4b5bab6075a1671045:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery:10901504:09b9ea9ca34c0b086701d15d270453f404b6b1e2; then
+  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/platform/msm_sdcc.1/by-name/boot:8151040:090d8248b641715f52a908903dc61d890c3bce65 EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery 09b9ea9ca34c0b086701d15d270453f404b6b1e2 10901504 090d8248b641715f52a908903dc61d890c3bce65:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
